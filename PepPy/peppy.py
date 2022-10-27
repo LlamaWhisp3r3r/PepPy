f8a678c198074f7ee3b7e7709763bd87b5598341
Covert-Law-Enforcement <96259081+Covert-Law-Enforcement@users.noreply.github.com>
2022-10-27 14:00:10 -0600

    Add ReadTimeout to requests function

--- PepPy/peppy.py
+++ PepPy/peppy.py
@@ -79,6 +79,8 @@ class PepPy:
                 response = requests.post(url, verify=False, cookies=self.cookies, timeout=self.__timeout, **kwargs)
         except requests.exceptions.ConnectTimeout:
             response = dict()
+        except requests.exceptions.ReadTimeout:
+            pass
 
         self.__check_for_new_cookies_in_reponse(response)
         return response
